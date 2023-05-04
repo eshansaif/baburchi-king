76ad9ea7acbae43f8031f6f0779e1f253d6c725a
eshansaif <35582062+eshansaif@users.noreply.github.com>
2023-05-04 12:53:06 +0600

    banner added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,8 +2,17 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            <h2>This is Home</h2>
+        <div className="">
+            <div className="hero min-h-screen" style={{ backgroundImage: `url("https://d3ox4wjkl7mf3m.cloudfront.net/feed_story/step/yCGo6Rzz3nsf4n9WrquZb3gEHxjaqEZqH5zhSo8h.jpeg")` }}>
+                <div className="hero-overlay bg-opacity-60"></div>
+                <div className="hero-content text-center text-neutral-content">
+                    <div className="max-w-md">
+                        <h1 className="mb-5 text-5xl font-bold">Traditional Bengali Cuisine</h1>
+                        <p className="mb-5">If you are intrigued about Bengali cuisine and want to know more than its explicitly famous mishti doi, then give a read to this blog!</p>
+                        <button className="btn btn-primary">Explore More</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
